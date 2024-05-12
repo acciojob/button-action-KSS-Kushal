b3655f65dceceb5e3dc628cce9001a6b00b21189
Kushal Ghosh <kushalghosh075@gmail.com>
2024-05-12 20:56:09 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,13 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
+  const [isHide, setIsHide] = useState(true);
 
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      {/* // Do not alter the main div */}
+      <p id="para" className={isHide?"hide":"show"}>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      <button id="click" onClick={()=>setIsHide(!isHide)}>Hide/Show</button>
     </div>
   );
 }
